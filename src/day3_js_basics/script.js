6a8445e048492fe76dd1f5e3cc910ef767632188
Tejas Kalmegh <tejaskalmegh123@gmail.com>
2025-10-04 08:11:53 +0530

    day3 files updated

---
+++ src/day3_js_basics/script.js
@@ -0,0 +1,13 @@
+console.log("Welcome to my first JavaScript Program...");
+
+let num1 = 10
+let num2 = 5
+const my_name = 'Tejas Kalmegh'
+
+console.log("Hello, I am ",my_name)
+
+console.log("Addition : ",num1+num2)
+console.log("Subtraction :",num1-num2)
+console.log("Multiplication :",num1*num2)
+console.log("Division :",num1/num2)
+
---
+++ src/tasks/day3_js_basics/script.js
@@ -0,0 +1,13 @@
+let my_name = "Tejas Kalmegh"
+const my_age = 24
+let student = false
+let num1 = 100
+let num2 = 50
+
+console.log("My Name is ",my_name);
+console.log("I am ",my_age,"year's old");
+console.log("Student :",student);
+console.log("Addition of Two numbers : ",num1+num2);
+
+
+
