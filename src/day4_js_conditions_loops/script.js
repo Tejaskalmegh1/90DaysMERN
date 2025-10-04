46b8bd1ca48d2742fafdf71c79eb2daa8c1ebdd8
Tejas Kalmegh <tejaskalmegh123@gmail.com>
2025-10-04 23:30:56 +0530

    day4 files updated

---
+++ src/day4_js_conditions_loops/script.js
@@ -0,0 +1,23 @@
+
+
+let num1 = 10
+
+if(num1 % 2 == 0){
+    console.log("Number is Even");
+}else{
+    console.log("Number is odd!");
+}
+
+prompt()
+{
+const num = prompt("Enter your Number : ")
+
+if(num > 0) {
+    console.log("Number is Possitive")
+} else if(num < 0) {
+    console.log("Number is Negative")
+} else {
+    console.log("Number is Zero!")
+}
+
+}
